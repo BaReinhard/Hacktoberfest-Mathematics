18e6d541c5406f58d0bfcc911c58f4ff0fe2b1b1
Ian W <ian.uu12@gmail.com>
2017-10-01 15:33:35 -0700

    Left-handed Riemann sum

---
+++ python/calculus/riemann.py
@@ -0,0 +1,15 @@
+from decimal import Decimal
+from numbers import Real
+from typing import Callable, Union
+
+number_arg = Union[Real, str] # Decimals can take any number, or a string
+
+def riemann_sum(func: Callable[[Decimal], number_arg], x1: number_arg, x2: number_arg, dx: number_arg):
+	accum = Decimal(0)
+	x1, x2, dx = map(Decimal, (x1, x2, dx)) # Convert all arguments to decimal for accuracy
+	x = x1
+	while x < x2:
+		y = Decimal(func(x))
+		accum += y * dx
+		x += dx
+	return accum
