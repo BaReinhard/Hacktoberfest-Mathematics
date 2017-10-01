4040e15886fb2f852efb4694f41501065d9fd63a
Michael E Zazula <zazulam@kean.edu>
2017-10-01 19:01:43 -0400

    Update gravitationalForceEquation.java
    edited the comments

--- java/physics/force/gravitationalForceEquation.java
+++ java/physics/force/gravitationalForceEquation.java
@@ -1,19 +1,20 @@
 Public class gravitationalForceEquation{
-		//There exists the gravitational constant of the universe G, 6.67 * 10^-11
+	//Meant to calculate teh gravitational force between two objects
+	//There exists the gravitational constant of the universe G, 6.67 * 10^-11
         
-		public static double final g = .0000000000667;
+	public static double final g = .0000000000667;
         
-        /*@param m1 corresponds to the mass of the first object
-         *@param m2 corresponds to the mass of the second object
-         *@param r corresponds to the distance between both masses
+        /*@param m1 corresponds to the mass of the first object in kgs
+         *@param m2 corresponds to the mass of the second object in kgs
+         *@param r corresponds to the distance between both masses in meters
          */
         
         
-        public double gravitationalForce(double m1, double m2, double r){
+        public static double gravitationalForce(double m1, double m2, double r){
         
-               double force = (g*m1*m2)/(r*r);
+               double gforce = (g*m1*m2)/(r*r);
                
-               return force;
-		}
+               return gforce;
+	}
 		
 }
