1dd4071255260eab86fcb63ba806a4fd005fc905
Minya <rancic.minya@gmail.com>
2017-10-01 22:56:25 -0400

    Merged with Original Repo

--- java/Reimann_sum/Driver.java
+++ java/Reimann_sum/Driver.java
@@ -5,8 +5,8 @@ public class Driver
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		Equation eq = new Equation("x^2 + 2*x + 1");
-		System.out.println(eq.evaluateElement(0, 0));
+		Equation eq = new Equation("x^4 - x^3 + x^2 - x + 26");
+		System.out.println(eq.evaluateEquation(26.0));
 	}
 
 }
