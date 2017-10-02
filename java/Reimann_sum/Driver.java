8792073644444b4afc31ee33d1b6f433077337b6
Minya <rancic.minya@gmail.com>
2017-10-01 23:28:49 -0400

    Ignore

--- java/Reimann_sum/Driver.java
+++ java/Reimann_sum/Driver.java
@@ -5,8 +5,9 @@ public class Driver
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
-		Equation eq = new Equation("x^4 - x^3 + x^2 - x + 26");
-		System.out.println(eq.evaluateEquation(26.0));
+		Equation eq = new Equation("x^2");
+		
+		System.out.println(ReimannSum.midpointReimannSum(0, 4, 500000, eq));
 	}
 
 }
