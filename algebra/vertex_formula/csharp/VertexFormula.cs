72adb7739496042f0b3e2536f4070ee1ad4f8f1e
Brett Reinhard <brettreinhard@gmail.com>
2017-10-02 15:58:22 -0700

    Update VertexFormula.cs

--- algebra/vertex_formula/csharp/VertexFormula.cs
+++ algebra/vertex_formula/csharp/VertexFormula.cs
@@ -4,12 +4,12 @@ namespace csharp
 {
     class VertexFormula
     {
-        public static double GetVertex(double termA, double termB, double termC = 0.0)
+        public static (double,double) GetVertex(double termA, double termB, double termC = 0.0)
         {
             if(termA == 0.0)
                 throw new Exception("Divide by zero error. Cannot use zero for termA in GetVertex function");
             else
-                return (-1 * termB) / (2 * termA);
+                return (((-1 * termB) / (2 * termA)),((4*termA*termC-termB*termB)/(4*termA)));
         }
     }
 }
