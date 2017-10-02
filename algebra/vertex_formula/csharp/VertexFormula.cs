f49f39f9752b3e09150d55ebac19a7a29e489d66
Casey Brown <casey.92.brown@gmail.com>
2017-10-01 18:07:47 -0700

    Added VertexFormula

---
+++ algebra/vertex_formula/csharp/VertexFormula.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp
+{
+    class VertexFormula
+    {
+        public static double GetVertex(double termA = 0.0, double termB = 0.0, double termC = 0.0)
+        {
+            if(termA == 0.0 | termB == 0.0)
+                return 0.0;
+            else
+                return (-1 * termB) / (2 * termA);
+        }
+    }
+}
