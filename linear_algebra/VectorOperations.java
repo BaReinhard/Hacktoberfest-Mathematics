7ec0a8b24240b284a9f5473228f7589310e9919e
Nina029 <32497099+Nina029@users.noreply.github.com>
2017-10-04 00:46:28 +0200

    vector sum

--- linear_algebra/VectorOperations.java
+++ linear_algebra/VectorOperations.java
@@ -8,10 +8,26 @@ public class VectorOperations {
         double sum=0.0;
         double length = 0.0;
        for(int i =0; i<vector.length; i++){
-           sum+=Math.pow(vector[i], 2.0));  
+           sum+=Math.pow(vector[i], 2.0);  
        }
      
       length = Math.sqrt(sum);
       return length;
+    }
+    
+    public static double[] vectorSum (double[] vector1, double[] vector2){
+    
+    
+        if(vector1.length==vector2.length){
+          double[] sumVector = new double[vector1.length];
+        	for(int i = 0; i<vector1.length; i++){
+        	  sumVector[i]=vector1[i]+vector2[i];
+        	}
+        return sumVector;
+        }else{
+          System.out.println("Dimensions of vectors do not match!");
+          return null;
+        }
+    
     }
 }
