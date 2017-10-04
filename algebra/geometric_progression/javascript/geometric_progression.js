a3b3c7831c8b1519083956d1e3eca686039d0bf8
Golfo Vasiliou <faysvas@yahoo.com>
2017-10-05 00:06:11 +0300

    Improved styling

--- algebra/geometric_progression/javascript/geometric_progression.js
+++ algebra/geometric_progression/javascript/geometric_progression.js
@@ -5,6 +5,7 @@
 function nth_term(a, r, n){
 	return  a * Math.pow(r, n - 1);
 }
+
 // This function calculates the sum of the first n terms
 function sum_of_first_n(a, r, n){
 	return (a * (1-Math.pow(r,n))) / (1 - r);
