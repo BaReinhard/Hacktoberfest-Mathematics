d379c8a610b4f7ed763c258abf1cd2bfa79bfbc7
prudywsh <antoineprudhomme5@gmail.com>
2017-10-08 11:20:55 +0200

    prime factorization function upgraded

--- algebra/prime_factorization/python/prime_factorization.py
+++ algebra/prime_factorization/python/prime_factorization.py
@@ -1,17 +1,5 @@
 from math import sqrt
 
-def is_prime(n):
-	""" A number is prime if it can be devide only by 1 and himself
-		Check from 2 to sqrt(n). After sqrt(n) the numbers are redondant
-	"""
-	if n == 1:
-		return False
-
-	for i in range(2, int(sqrt(n))):
-		if n % i == 0:
-			return False
-	return True
-
 def prime_factorization(n, primes = []):
 	""" Find the prime numbers (recursively) so that
 		n = p1^a1 * p2^a2 * ... * pn^an
@@ -20,7 +8,7 @@ def prime_factorization(n, primes = []):
 		return primes
 
 	i = 2
-	while not (is_prime(i) and n % i == 0):
+	while not n % i == 0:
 		i += 1
 	primes.append(i)
 	return prime_factorization(n // i, primes)
