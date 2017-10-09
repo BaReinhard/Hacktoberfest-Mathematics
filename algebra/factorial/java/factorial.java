ed02e2164722c64c77e52eff0e3f4a291d9936cf
Lucas <Lucasvancleef@gmail.com>
2017-10-09 19:42:30 -0400

    Alternative java factorial method without the use of recursion

--- algebra/factorial/java/factorial.java
+++ algebra/factorial/java/factorial.java
@@ -1,11 +1,23 @@
-class factorial{
-	
-	public static int factorial(int n){
-		if(n == 0){
-			return 1;
-		} else{
-			return n * factorial(n-1);
-		}
-	}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.factorial;
 
+/**
+ *
+ * @author Lucas
+ */
+public class Factorial {   
+    
+    public static int factor(int n){
+        int product = 1;
+        
+        for (int i = 1; i <= n; i++ ){
+           product = product*i;
+        }
+        
+        return product;
+    }
 }
