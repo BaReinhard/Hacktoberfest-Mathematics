5b765e271447600fafcaa9e3899007db1919e617
Sarthak Bhagat <sarthakbhagat@gmail.com>
2017-10-13 21:07:02 +0530

    Update CircleFormula.py

--- geometry/circles/python/CircleFormula.py
+++ geometry/circles/python/CircleFormula.py
@@ -1,3 +1,5 @@
+import math
+
 ##Returns True if the point lies on the circle, else false
 def isPointOnCircle(center_x, center_y, radius, point_x, point_y):
     return (point_x-center_x)**2+(point_y-center_y)**2-radius**2==0
@@ -6,4 +8,6 @@ def isPointOnCircle(center_x, center_y, radius, point_x, point_y):
 def getRadius(center_x, center_y, point_x, point_y):
     return ((point_x-center_x)**2+(point_y-center_y)**2)**(1/2)
 
-
+##returns the length of arc given the angle in degrees and radius
+def arc_length(radius,angle):
+    return (angle*radius*math.pi*(1/180))
