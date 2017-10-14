bd1e78dcb9503f9f040971f721baf4b9443e044b
Sarunyu Chankong <15183551+mayueeeee@users.noreply.github.com>
2017-10-15 00:47:47 +0700

    Add energy equations in JS

---
+++ physics/energy_equations/javascript/energy_equations.js
@@ -0,0 +1,10 @@
+function pot_energy(height,mass){
+    return mass*9.81*height
+}
+
+function kinetic_energy(mass,speed){
+    return 0.5*mass*speed*speed
+}
+function power (work,time){
+    return work/time
+}
