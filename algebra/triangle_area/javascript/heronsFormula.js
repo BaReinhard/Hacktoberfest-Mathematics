6637bc87f69cf5ea9c5df1a78c9d8c535655858a
Hum4n01d <hum4n01d@icloud.com>
2017-10-19 18:49:07 -0700

    More verbose and added ES6

--- algebra/triangle_area/javascript/heronsFormula.js
+++ algebra/triangle_area/javascript/heronsFormula.js
@@ -1,7 +1,13 @@
-function triangleArea(a, b, c) {
-  var S = ((a + b + c) / 2)
-  
-  var A = Math.sqrt(S*(S-a)*(S-b)*(S-c))
+// ES5 way
+function triangleAreaES5(a, b, c) {
+  var halfPerimeter = ((a + b + c) / 2)
 
-  return A
+  return Math.sqrt(halfPerimeter*(halfPerimeter-a)*(halfPerimeter-b)*(halfPerimeter-c))
+}
+
+// ES6 way
+const triangleAreaES6 = (a, b, c) => {
+  const halfPerimeter = ((a + b + c) / 2)
+
+  return Math.sqrt(halfPerimeter*(halfPerimeter-a)*(halfPerimeter-b)*(halfPerimeter-c))
 }
