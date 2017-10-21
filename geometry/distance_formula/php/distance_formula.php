9018548a80e4b1fcfe6bbd52c7771eb5cd76dabf
brainmaniac <brainmaniac@users.noreply.github.com>
2017-10-21 12:20:56 +0200

    missed deleting stuff

--- geometry/distance_formula/php/distance_formula.php
+++ geometry/distance_formula/php/distance_formula.php
@@ -9,20 +9,4 @@ function euclidianDistance($p1,$p2)
     return $distance;
 };
 
-function test() {
-    $p1 = {
-        "x" : 0,
-        "y" : 0
-    };
-
-    $p2 = {
-        "x" : 1,
-        "y" : 1
-    };
-
-    return euclidianDistance(p1,p2);
-}
-
-alert test();
-
 ?>
