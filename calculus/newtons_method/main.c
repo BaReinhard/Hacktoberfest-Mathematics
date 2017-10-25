d6c2577847f541e14b5a670defc9f7f7cdb8f6f5
cfabian <cfabian150@gmail.com>
2017-10-25 20:17:36 +0000

    finished newtons method

--- calculus/newtons_method/main.c
+++ calculus/newtons_method/main.c
@@ -1,13 +1,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+#include <ctype.h>
 
-double evalFunc(double x) {
-    return (1 * pow(x, 2) + 7 * pow(x, 1) + 4);
-}
-
-double evalDeriv(double x) {
-    return (2 * pow(x, 1) + 7);
+double evalFunc(double x, char *fun) {
+    
+    double coefficient = 0.0;
+    double exponent = 0.0;
+    double total = 0.0;
+    
+    int ex = 0;
+    
+    char *funParams = calloc(strlen(fun) + 1, sizeof(char));
+    strncpy(funParams, fun, strlen(fun));
+    funParams = strtok(funParams, ",;");
+    while (funParams != NULL) {
+        if (ex) {
+            exponent = atof(funParams);
+            total += coefficient * pow(x, exponent);
+            ex = 0;
+        } else {
+            coefficient = atof(funParams);
+            ex = 1;
+        }
+        funParams = strtok(NULL, ",;");
+    }
+    
+    return total;
 }
 
 int main() {
@@ -15,21 +35,25 @@ int main() {
     char *derivative = calloc(100 * sizeof(char), sizeof(char));
     double startVal = 0;
     int reps = 0;
-    // printf("\nEnter a function of x (x^2+7x+4): ");
-    // scanf("%s", function);
-    // printf("\nEnter the derivative of the function (2x+7): ");
-    // scanf("%s", derivative);
+    printf("Function Format: coefficient,exponent;coefficient,exponent\nx^6+-2.5x^5+4x^3-17x+8 => 1,6;-2.5,5;4,3;-17,1;8,0\n");
+    printf("\nEnter a polynomial function of x in the described format: ");
+    scanf("%s", function);
+    printf("\nEnter the derivative of the function of x in the described format: ");
+    scanf("%s", derivative);
     printf("\nEnter a starting value (int): ");
     scanf("%lf", &startVal);
     printf("\nEnter the max number of repetitions (int): ");
     scanf("%d", &reps);
-    
-    // printf("%f, %f\n", evalFunc(5), evalDeriv(5));
+    printf("\n");
     
     int i = 0;
     double prevVal = startVal;
+    double funTotal = 0.0;
+    double derTotal = 0.0;
     for (i = 0; i < reps; i ++) {
-        startVal = (startVal - (evalFunc(startVal) / evalDeriv(startVal)));
+        funTotal = evalFunc(startVal, function);
+        derTotal = evalFunc(startVal, derivative);
+        startVal = (startVal - (funTotal / derTotal));
         printf("%d: %f\n", i, startVal);
         
         if (prevVal == startVal) {
@@ -39,7 +63,7 @@ int main() {
         }
     }
     
-    printf("Final Value: %f\n", startVal);
+    printf("Final Value: %.2f\n", startVal);
     
     return 0;
 }
