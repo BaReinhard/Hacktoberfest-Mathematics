40f91f7fa5346ec643a3784db5b01a1bde8c3c7c
Lawton Mizell <alcamech@gmail.com>
2017-10-26 11:50:16 -0400

    reduced row echelon in python

---
+++ linear_algebra/matrix_operations/python/reduced_row_echelon.py
@@ -0,0 +1,26 @@
+def ToReducedRowEchelonForm(M):
+    if not M: return
+    lead = 0
+    rowCount = len(M)
+    columnCount = len(M[0])
+    for r in range(rowCount):
+        if lead >= columnCount:
+            return
+        i = r
+        while M[i][lead] == 0:
+            i += 1
+            if i == rowCount:
+                i = r
+                lead += 1
+                if columnCount == lead:
+                    return
+        M[i],M[r] = M[r],M[i]
+        lv = M[r][lead]
+        M[r] = [ mrx / float(lv) for mrx in M[r]]
+        for i in range(rowCount):
+            if i != r:
+                lv = M[i][lead]
+                M[i] = [ iv - lv*rv for rv,iv in zip(M[r],M[i])]
+        lead += 1
+    for rw in mtx:
+        print ', '.join( (str(rv) for rv in rw) )
