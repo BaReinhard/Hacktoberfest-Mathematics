db0fa8719dc3c2d72c3718c7023f957fd5afcb31
osandadeshan <osanda.deshan@gmail.com>
2017-10-26 21:19:44 +0530

    Returning the value of hypotenuse of 4 and 3. Hypotenuse is the square root of a set of values. Here hypotenuse for 4 and 3 is the square root of (4x4 + 3x3) = 5

---
+++ trigonometry/hypotenuse/hypotenuse.py
@@ -0,0 +1,13 @@
+# Python code to demonstrate the working of hypot()
+  
+# importing "math" for mathematical operations
+import math
+ 
+a = 4
+b = 3
+  
+# returning the value of hypotenuse of 4 and 3
+# hypotenuse is the square root of a set of values
+# here hypotenuse for 4 and 3 is the square root of (4x4 + 3x3) = 5
+print ("The value of hypotenuse of 4 and 3 is : ", end="")
+print (math.hypot(a,b))
