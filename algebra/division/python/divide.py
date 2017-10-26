77ba749aa24a0d14daecf24e6f2db627bc237568
karlyttle <33133617+karlyttle@users.noreply.github.com>
2017-10-26 12:24:20 -0700

    Create divide.py

---
+++ algebra/division/python/divide.py
@@ -0,0 +1,9 @@
+def divide(first,second):
+  """Divides the second number from the first"""
+  if second == 0:
+    print str(second) + " cannot be zero"
+  else
+    return first/second
+
+if __name__ == "__main__":
+  divide(3,3)
