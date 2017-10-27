4376590ff1bb8c1779963c63b3faf36ebbd8d1b1
Vikas568 <vikas.reddem568@gmail.com>
2017-10-27 11:46:33 +0530

    Update arithmetic_progression.java

--- algebra/arithmetic_progression/java/arithmetic_progression.java
+++ algebra/arithmetic_progression/java/arithmetic_progression.java
@@ -9,7 +9,10 @@ public class arithmetic_progression {
 
     // This function calculates the sum of the first n terms
     public static int sum_of_first_n(int a, int d, int n) {
-        return ((n) * ((2 * a) + (n - 1) * d)) / 2;
+        
+        //first we have make the calculation simple so that its value doesn't get out of integer range
+        // so we have to devide the some part of number with 2 so that it becomes small and after that multiply with other 
+        return n * (((2 * a) + (n - 1) * d)/2);
     }
 
 }
