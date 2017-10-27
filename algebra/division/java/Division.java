4d2e20a97b577cc2c8af40b9ccf2bc02f5148c43
Mujahid Anuar <orafum96@gmail.com>
2017-10-27 18:58:58 -0500

    Add exceptions to "divide by zero" error

--- algebra/division/java/Division.java
+++ algebra/division/java/Division.java
@@ -1,11 +1,11 @@
-/* Division in integer only, implemented in Java.
+/* Division in integer implemented in Java.
 *
 * Takes in 2 integer arguments, x is the numerator, y is denominator
 * - Format: java Division [numerator] [denominator]
+* - will only return the division, no remainder
 *
 * @author: Mujahid Anuar, github.com/mjhdanuar
 */
-
 import java.io.*;
 
 public class Division {
@@ -15,18 +15,22 @@ public class Division {
     try {
       Integer.parseInt(args[0]);
       Integer.parseInt(args[1]);
+      
     } catch (NumberFormatException e) {
       System.out.println("Please input integers only.");
       System.exit(0);
     }
 
     if (args.length == 2) {
-      int result = divide(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-
-      if (result != 0)
+      
+      // check for divide by zero
+      try {
+        int result = divide(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println(result);
-      else
+
+      } catch (ArithmeticException e) {
         System.out.println("Cannot divide by zero.");
+      }
 
     }
     else {
@@ -37,12 +41,17 @@ public class Division {
 
 /* Divides two integers
 * If denominator, y, is zero, return 0
-*
+* 
+* @param x, y 
+*   x is numerator, y is denominator
+* @return the division, no remainder
 */
   public static int divide(int x, int y) {
-    if (y == 0)
-      return 0;
-
+    // check for divide by zero
+    if (y == 0) {
+      throw new ArithmeticException();
+    }
+    
     return x/y;
   }
 }
