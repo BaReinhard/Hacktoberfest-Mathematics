b430b0e11bd8fcb288a4be06b152b56cc049ff2d
praveenmylavarapu <saipraveenmylavarapu@gmail>
2017-10-28 15:38:22 +0530

    Added subtract Matrix

--- linear_algebra/matrix_operations/python/matrix_operations.py
+++ linear_algebra/matrix_operations/python/matrix_operations.py
@@ -6,6 +6,13 @@ def add_matrix(A, B):
             C[i][j] = A[i][j] + B[i][j]
     return C
 
+def subtract_matrix(A, B):
+    assert len(A) == len(B) and len(A[0]) == len(B[0])
+    C = [[0]*len(A[0]) for i in range(len(A))]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            C[i][j] = A[i][j] - B[i][j]
+    return C
 
 def multiply_matrix(A, B):
     assert len(A[0]) == len(B)
