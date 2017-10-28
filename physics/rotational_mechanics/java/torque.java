a694c9da19bfb59048ccab06a28f493f70e4c681
connewm <32915683+connewm@users.noreply.github.com>
2017-10-27 20:03:58 -0400

    Create torque.java

---
+++ physics/rotational_mechanics/java/torque.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+class torque {
+  /*
+   *@param a - the angular acceleration
+   *@param i - the moment of inertia
+   *@return t - the value of torque
+   */
+  public static double calcTorque(double a, double i) {
+    double t = (a * i);
+    return t;
+  }
+  /*
+   *@param r - the distance from axle to force applied
+   *@param f - the force applied 
+   *@param angle - the angle between r and f in degrees
+   *@return t - the resulting torque
+   */
+  public static double calcTorque(double r, double f, double angle) {
+     angle = Math.toRadians(angle);
+     double sinAngle = Math.sin(angle);
+     double t = (r * f * sinAngle);
+     return t;
+   }
+ }
