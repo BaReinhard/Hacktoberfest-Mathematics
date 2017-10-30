50282cb4f889608c1c9b566b3b2ed3883f2acfc5
Lucas Moutinho <lucasmoutinho>
2017-10-29 23:20:28 -0200

    Subtraction in C

---
+++ algebra/subtraction/c/subtract.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  int num1, num2;
+
+  printf("Enter the first number:\n");
+  scanf("%d", &num1);
+  printf("Enter the second number:\n");
+  scanf("%d", &num2);
+  printf("%d - %d = %d\n", num1, num2, num1-num2);
+  return 0;
+}
