a46baf95d33c1327e07c17e4598a191d1e010220
junaidrahim <junaidrahim8d@gmail.com>
2018-10-01 17:58:52 +0530

    algebra/addition | added add.go

---
+++ algebra/addition/golang/add.go
@@ -0,0 +1,15 @@
+// Code to add 2 numbers
+
+package main
+
+import "fmt"
+
+func add (a int,b int) int {
+	return a+b;
+}
+
+func main(){
+	fmt.Print("The sum of 2 and 3 is ")
+	fmt.Println(add(2,3))
+}
+
