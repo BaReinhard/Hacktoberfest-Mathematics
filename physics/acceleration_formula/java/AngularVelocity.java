61493f9f59b7ef377843bd9310c55dc8a61cc1ee
RenFraser <henry.fraser01@bigpond.com>
2018-10-04 12:54:38 +1000

    Added angular velocity.

---
+++ physics/acceleration_formula/java/AngularVelocity.java
@@ -0,0 +1,11 @@
+/*
+ * Calculate the angular velocity with tangential
+ * velocity and also angular acceleration.
+*/
+public class AngularVelocity {
+
+    // Perform the calculation.
+    public static double calcualte(double velocity, double angularAcceleration) {
+        return velocity/angularAcceleration;
+    }
+}
