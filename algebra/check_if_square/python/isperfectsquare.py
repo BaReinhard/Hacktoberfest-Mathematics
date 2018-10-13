5285e1745d2d820e228a653c4dbb986c8c3231c1
issilva5 <39525169+issilva5@users.noreply.github.com>
2018-10-13 17:01:01 -0300

    Create isperfectsquare.py

---
+++ algebra/check_if_square/python/isperfectsquare.py
@@ -0,0 +1,5 @@
+number = int(raw_input("Enter a number: "))
+if int(number ** (0.5)) == number ** (0.5):
+  print str(number) + " is a perfect square number."
+else:
+  print str(number) + " is not a perfect square number."
