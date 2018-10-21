e1d56e39aedf0cb14e10635253132e2534265cda
Logan Green <loganagreen95@gmail.com>
2018-10-21 18:17:52 -0400

    Add ruby geometry triangles, and perimeter, area, type

---
+++ geometry/triangles/ruby/Triangle.rb
@@ -0,0 +1,17 @@
+def area(base, height):
+    return (base*height)/2
+end
+
+def perimeter(side1, side2, side3)
+    side1 + side2 + side3
+end
+
+def triside_type(side1, side2, side3)
+    if [side1, side2, side3].uniq.count == 1
+        return "equilateral"
+    else [side1, side2, side3].uniq.count == 2
+            return "isosceles"
+    else
+            return "scalene"
+    end
+end
