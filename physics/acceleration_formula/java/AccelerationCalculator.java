b27c9971ad05a71596d83d5277820c329156ea7f
Aayushthe1 <43931514+Aayushthe1@users.noreply.github.com>
2018-10-21 19:40:27 +0530

    Update AccelerationCalculator.java

--- physics/acceleration_formula/java/AccelerationCalculator.java
+++ physics/acceleration_formula/java/AccelerationCalculator.java
@@ -8,7 +8,7 @@ public class AccelerationCalculator {
 	 * @param u - Initial Velocity in meter/second
 	 * @param v - Final Velocity in meter/second
 	 * @param t - Time taken in seconds
-	 * @return acceleration in meter/second^2
+	 * @return acceleration in meters/second^2
 	 */
 	public static double calculateFor(double u, double v, double t) {
 		return (v - u) / t;
