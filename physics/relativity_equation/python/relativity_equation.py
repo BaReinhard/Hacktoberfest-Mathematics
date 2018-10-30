eb446f42de461d9f62436b825d8d0438679968ab
noblesword <30838498+noblesword@users.noreply.github.com>
2018-10-30 22:09:32 +0530

    updtaed some more function

--- physics/relativity_equation/python/relativity_equation.py
+++ physics/relativity_equation/python/relativity_equation.py
@@ -1,5 +1,12 @@
 # Einstein's Special Relatvity Equation( change in time, mass ,momentum of an object moving at a speed closer to the speed of light 3x 10^8 m/s)
-#Let
+#Let the parameters be represented by these acronyms
+# mass = Mass of Object
+# c = speed of Light
+# vel = velocity of object
+# t0 = Initial time
+# l0 = initial length
+
+c= 3*pow(10,8)
 def E(mass):
       return mass*(c**2)
 
@@ -7,4 +14,7 @@ def del_T(vel,t0)
       return (to/pow((1-(vel*vel)/(c*c)),0.5))
 def del_L(l0,vel)
       return (lo*pow((1-(vel*vel)/(c*c)),0.5))
+def real_E(mass, vel)
+      return ((m*c*c)/(lo*pow((1-(vel*vel)/(c*c)),0.5))
+
 
