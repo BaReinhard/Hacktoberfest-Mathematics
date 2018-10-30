b3bf8d77793bbeca778af51fbbff21a223ca7568
noblesword <30838498+noblesword@users.noreply.github.com>
2018-10-30 21:50:11 +0530

    Change in time, length
    When an object nears the speed of light, according to Einstein's theory of relativity, the time and length changes as per the equations defined.

--- physics/relativity_equation/python/relativity_equation.py
+++ physics/relativity_equation/python/relativity_equation.py
@@ -1,3 +1,10 @@
-# Einstein's Special Relatvity Equation(e=mc2)
-def relativity_eqn(mass):
+# Einstein's Special Relatvity Equation( change in time, mass ,momentum of an object moving at a speed closer to the speed of light 3x 10^8 m/s)
+#Let
+def E(mass):
       return mass*(c**2)
+
+def del_T(vel,t0)
+      return (to/pow((1-(vel*vel)/(c*c)),0.5))
+def del_L(l0,vel)
+      return (lo*pow((1-(vel*vel)/(c*c)),0.5))
+
