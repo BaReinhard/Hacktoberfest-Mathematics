6f68cb06ca6f2ef2f9eeef9aeaf2b09c19bc8153
Keith Wade <keawade@protonmail.com>
2018-10-31 19:11:28 -0500

    Physics force equation in javascript

---
+++ physics/force_equation/javascript/force.js
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Calculates force given mass and acceleration.
+ * @param {number} mass
+ * @param {number} acceleration
+ * @returns {number}
+ */
+export const force = (mass, acceleration) => {
+  return mass * acceleration;
+};
