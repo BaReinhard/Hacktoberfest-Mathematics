01101b1ffbb776030a6b9b066d0650cceda19c5e
Abhishek <prajapatia2580.com>
2019-10-01 12:56:53 -0700

    Abhishek

---
+++ algebra/arithmetic_progression/python/arithmetic progression.py
@@ -0,0 +1,18 @@
+
+def printAP(a,d,n): 
+ 
+	curr_term 
+curr_term=a 
+
+	for i in range(1,n+1): 
+print(curr_term, end=' ') 
+		curr_term =curr_term + d 
+		
+
+
+ 
+a=int(input())
+d=int(input())
+n=int(input())
+printAP(a, d, n) 
+
