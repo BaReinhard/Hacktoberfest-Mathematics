547ce50314162cc09500fddda94d371edbf340c8
Scout Wallace <46305235+scouttww@users.noreply.github.com>
2019-10-23 16:49:59 -0400

    Update LinearFunction.java to include yIntercept()
    Included yIntercept() method to evaluate and return yIntercept. Also modified slope to include a provision for vertical lines, thereby preventing division by zero

--- algebra/linear_functions/java/LinearFunctions.java
+++ algebra/linear_functions/java/LinearFunctions.java
@@ -2,6 +2,11 @@ public class LinearFunctions {
     
     public static double slope(double x1, double y1, double x2, double y2){
         
+        if (x1 == x2){ // vertical line has a NaN/undetermined slope
+            System.out.println("Line is vertical. It has an undetermined slope");
+            return null;
+        }
+        
         return (y2-y1)/(x2-x1);
         
     }
@@ -11,5 +16,19 @@ public class LinearFunctions {
         return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
         
     }
+    
+    public static double yIntercept(double x1, double y1, double x2, double y2){
+        
+        double slope = slope(x1, y1, x2, y2);
+        
+        if (slope != null){ // assuming not a vertical line
+            return (y1-slope*x1);
+        } else {
+            System.out.println("Line is vertical. It has no y intercept");
+            return null;
+        }
+    }
+        
+        
     
 }
