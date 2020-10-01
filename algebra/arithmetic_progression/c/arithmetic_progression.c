3aa55f935d06b2b996e94a0e7053a53ff3a6d257
Pranay711 <71118731+Pranay711@users.noreply.github.com>
2020-10-01 22:55:20 +0530

    Update arithmetic_progression.c
    syntax and algorthmic changed.

--- algebra/arithmetic_progression/c/arithmetic_progression.c
+++ algebra/arithmetic_progression/c/arithmetic_progression.c
@@ -1,26 +1,25 @@
-#include <stdio.h>
-#include <math.h>
- 
+#include <bits/stdc++.h>
 int main()
 {
-     int a, d, n, i, tn;
+     int a, d, n, tn;
      int sum = 0;
  
      printf("Enter the first term value of the A.P. series: ");
      scanf("%d", &a);
-     printf("Enter the total numbers in the A.P. series: ");
+     printf("Enter the total numbers of terms in the A.P. series: ");
      scanf("%d", &n);
      printf("Enter the common difference of A.P. series: ");
      scanf("%d", &d);
-     sum = (n * (2 * a + (n - 1)* d ))/ 2;
+     // nth term
      tn = a + (n - 1) * d;
+     //sum of first n terms using formula
+     sum = (n * (a + tn))/ 2;
+     // sum of first n term by add term by term
      printf("Sum of the A.P series is: ");
-     for (i = a; i <= tn; i = i + d )
+     while(a <= tn)
      {
-          if (i != tn)
-               printf("%d + ", i);
-          else
-               printf("%d = %d ", i, sum);
+       a=a+d;
      }
+      printf("%d", a)
      return 0;
 }
